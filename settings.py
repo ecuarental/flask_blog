6479b4f2c70637019ebc3e8fcbf81bacd8d8b61e
Enrique Cuarental <e.cuarental@gmail.com>
2017-12-14 16:19:45 +0100

    Set SQLAlchemy Tracking to True

--- settings.py
+++ settings.py
@@ -10,3 +10,4 @@ DB_HOST = '127.0.0.1'
 DB_URI = 'mysql+pymysql://%s:%s@%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOST,
                                           BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
