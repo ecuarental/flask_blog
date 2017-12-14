7a61570f910e566b015d2b599d06aec60648862c
Enrique Cuarental <e.cuarental@gmail.com>
2017-12-14 18:23:41 +0100

    Create author register webpage with form

---
+++ author/form.py
@@ -0,0 +1,22 @@
+"""Author Forms."""
+
+from flask_wtf import Form
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+
+class RegisterForm(Form):
+    """Register Form."""
+
+    fullname = StringField('Full Name', [validators.Required()])
+    email = EmailField('Email', [validators.Required()])
+    username = StringField('Username', [
+        validators.Required(),
+        validators.Length(min=4, max=25)
+        ])
+    password = PasswordField('New password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.Length(min=4, max=80)
+    ])
+    confirm = PasswordField('Repeat password')
--- author/views.py
+++ author/views.py
@@ -1,9 +1,18 @@
 """Author views."""
 
 from flask_blog import app
+from flask import render_template, redirect
+from author.form import RegisterForm
 
 
 @app.route('/login')
 def login():
     """Homepage for user."""
     return 'Hello, user!'
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    """Register form."""
+    form = RegisterForm()
+    return render_template('author/register.html', form=form)
