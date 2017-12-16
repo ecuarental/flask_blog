3ecb6fcd40187a784b89467a595c8d064f10a746
Enrique Cuarental <e.cuarental@gmail.com>
2017-12-16 19:50:42 +0100

    Add Registration Form and Succesful Registration Form

--- author/views.py
+++ author/views.py
@@ -1,7 +1,7 @@
 """Author views."""
 
 from flask_blog import app
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for
 from author.form import RegisterForm
 
 
@@ -15,4 +15,12 @@ def login():
 def register():
     """Register form."""
     form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('sucess'))
     return render_template('author/register.html', form=form)
+
+
+@app.route('/sucess')
+def success():
+    """Succesful Registration."""
+    return "Succesful registration"
